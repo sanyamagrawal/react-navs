de482252199808a7fdcee8cb115493f550952b0c
SANYAM AGRAWAL <sanyam4u@gmail.com>
2015-06-04 10:08:15 +0530

    Nav Menues

--- app/js/app.js
+++ app/js/app.js
@@ -1,9 +1,54 @@
 /** @jsx React.DOM */
 
-var MessageBox = React.createClass({
-    render: function() {
-        return ( < h1 > Hello World < /h1>);
-    }
-});
+var MenuExample = React.createClass({
 
-React.renderComponent(<MessageBox /> , document.getElementById('app'));
+            getInitialState: function() {
+                return {
+                    focused: 0
+                };
+            },
+
+            clicked: function(index) {
+
+                // The click handler will update the state with
+                // the index of the focused menu entry
+
+                this.setState({
+                    focused: index
+                });
+            },
+
+            createNavNodes: function(itemName, index) {
+                var focusClass;
+                if (this.state.focused === index) {
+                    focusClass = "focused";
+                }
+
+                return (<li className={focusClass} onClick={this.clicked.bind(this,index)}>{itemName}</li>);
+            },
+
+            render: function() {
+
+                // Here we will read the items property, which was passed
+                // as an attribute when the component was created
+
+                var navNodes = this.props.items.map(this.createNavNodes);
+                // The map method will loop over the array of menu entries,
+                // and will return a new array with <li> elements.
+
+                return (
+                    <div>
+                        <ul> {navNodes}</ul>
+                        <p>
+                            Selected: {this.props.items[this.state.focused]}
+                        </p>
+                    </div >
+                );
+            }
+        });
+
+// Render the menu component on the page, and pass an array with menu options
+
+React.renderComponent( < MenuExample items = {
+    ['Home', 'Services', 'About', 'Contact us']
+} />, document.body);
